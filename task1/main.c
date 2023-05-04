56602ac0a2657fb84408a19bbbf93f992c684e7b
lmoroco <lmorocoramos@gmail.com>
2023-05-04 03:52:33 -0500

    Makefile generated

--- task1/main.c
+++ task1/main.c
@@ -3,15 +3,16 @@
 #include <string.h>
 #include <math.h>
 
-#define MAX_USERS 610
-#define MAX_BOOKS 193609
+#define MAX_USERS 2
 #define LINE 1024
-#define BOOKS_INPUT 9742
+#define BOOKS_INPUT 5
 #define BUFFER 200
 
-const int N = 10;
-char iFiLeName[8] = "out.txt";
-char iMovName[12] = "outmov.txt";
+// #define MAX_USERS 610
+// #define BOOKS_INPUT 9742
+
+char iFiLeName[10] = "test.txt";
+char iMovName[12] = "testmov.txt";
 
 char *sparTitle[BOOKS_INPUT + 1];
 int sparIndex[BOOKS_INPUT + 1];
@@ -110,8 +111,34 @@ float dotProductBetween(struct LinkedList *from, struct LinkedList *to) {
   return dot;
 }
 
+float manhattanBetween(struct LinkedList *from, struct LinkedList *to) {
+  float manh = 0.0f;
+  struct Node *prtFrom = from->root;
+  struct Node *ptrTo = to->root;
+
+  while (prtFrom != NULL && ptrTo != NULL) {
+    if (prtFrom->index == ptrTo->index) {
+      manh += fabs(prtFrom->rating - ptrTo->rating);
+      prtFrom = prtFrom->next;
+      ptrTo = ptrTo->next; 
+    } else if (prtFrom->index < ptrTo->index) {
+      prtFrom = prtFrom->next;
+    } else {
+      ptrTo = ptrTo->next;
+    }
+  } 
+
+  free(ptrTo);
+
+  return manh;
+}
+
 float queryDotProductBetween(struct SparseMatrixLinkedList *root, int src, int to) {
   return dotProductBetween(root->indexForRoots[src], root->indexForRoots[to]);
+}
+
+float queryManhattanBetween(struct SparseMatrixLinkedList *root, int src, int to) {
+  return manhattanBetween(root->indexForRoots[src], root->indexForRoots[to]);
 }
 
 void freeLL(struct LinkedList *head) {
@@ -141,6 +168,19 @@ struct Tuple computeBestCosineSimilarity(struct SparseMatrixLinkedList *root, in
     best = (similarity > best.similarity) ? (struct Tuple){similarity, i} : best;
   }
  
+  return best;
+}
+
+struct Tuple computeBestManhattan(struct SparseMatrixLinkedList *root, int src) {
+  struct Tuple best = {99999.0f, -1};
+  float distance = -1.0f;
+
+  for (int i = 1; i <= MAX_USERS; ++i) {
+    if (src == i) continue;
+    distance = queryManhattanBetween(root, src, i);
+    best = (distance < best.similarity) ? (struct Tuple){distance, i} : best;
+  }
+
   return best;
 }
 
@@ -191,7 +231,7 @@ int main() {
   FILE *iMov;
   iMov = fopen(iMovName, "r");
   if (iMov == NULL) {
-    perror("Error while opening the File!\n");
+    perror("Error while opening iMov the File!\n");
     exit(EXIT_FAILURE);
   }
  
@@ -213,7 +253,7 @@ int main() {
   FILE *iFile;
   iFile = fopen(iFiLeName, "r");
   if (!iFile) {
-    perror("Error while opening the File!\n");
+    perror("Error while opening iFile the File!\n");
     exit(EXIT_FAILURE);
   }
 
@@ -232,15 +272,23 @@ int main() {
 
   int option; 
   int idUser;
+  struct Tuple res;
   do {
-    printf("Select an option: \n1) Cosine Similarity\n4) Exit\n");
+    printf("Select an option: \n1) Cosine Similarity\n2) Manhattan Distance\n4) Exit\n");
     scanf("%d", &option);
 
     switch (option) {
       case 1:
         printf("Put the User Id: ");
         scanf("%d", &idUser);
-        struct Tuple res = computeBestCosineSimilarity(&matrix, idUser);
+        res = computeBestCosineSimilarity(&matrix, idUser);
+        printf("cos: %f - id user: %d\n", res.similarity, res.id);
+        break;
+
+      case 2:
+        printf("Put the User Id: ");
+        scanf("%d", &idUser);
+        res = computeBestManhattan(&matrix, idUser);
         printf("cos: %f - id user: %d\n", res.similarity, res.id);
         break;
       
@@ -254,5 +302,6 @@ int main() {
     }
   } while (option != 4);
 
+  freeMemory(&matrix);
   return 0;
 }
